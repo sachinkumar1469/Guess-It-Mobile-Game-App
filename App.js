17249d57e230ffaee9024f775275117d4527ece9
Sachin Kumar <91049993+sachinkumar1469@users.noreply.github.com>
2023-01-18 23:51:18 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -6,16 +6,18 @@ import {LinearGradient} from 'expo-linear-gradient'
 import StartGameScreen from './screens/StartGameScreen';
 import { useState } from 'react';
 import GameScreen from './screens/GameScreen';
+import GameoverScreen from './screens/GameoverScreen';
 
 export default function App() {
 
   const [number,setNumber] = useState();
+  const [ganmeOver,setGameOver] = useState(false);
 
   return (
     <LinearGradient colors={['#cf23cf','#ab00ab','#790079']} style={styles.container}>
       <ImageBackground source={require('./assets/background.png')} style={styles.img} resizeMode='cover' imageStyle={{opacity:0.3}}>
         <SafeAreaView style={styles.container}>
-          {!number ? <StartGameScreen setNumber={setNumber}/> : <GameScreen selectedValue={number}/>}
+          {!true ? <StartGameScreen setNumber={setNumber}/> : !true ? <GameScreen selectedValue={number} setGameOver={setGameOver}/> : <GameoverScreen/>}
         </SafeAreaView>
       </ImageBackground>
       <StatusBar style='inverted'/>
